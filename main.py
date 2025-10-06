031f6ee45a3dd3069a0074c01657560e9eb9d2f9
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-06 14:33:27 +0700

    Update scences

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
-
+#Connect files to charactor
+from stage.charactor import Globals
 @gdclass
 class main(Control):
 	# popup close
 	def _ready(self):
 		#ready game this pop up close
 		self.get_node("CanvasLayer").visible = False
+
 	def _on_newgame_pressed(self):
 		#open popup
 		self.get_node("CanvasLayer").visible = True
@@ -21,7 +23,7 @@ class main(Control):
 
 	def _on_closepop_pressed(self):
 		#close popup
-		self.get_node("CanvasLayer").visible = False
+		self.get_node("CanvasLayer").visible = False 
 
 	#exitgame
 	def _on_exitgame_pressed(self):
@@ -29,11 +31,14 @@ class main(Control):
 
 	#difficult select and var to scale with exp_gain and monster status 
 	def _on_easy_pressed(self):
-		difficult = 1
+		#add Globals. forward
+		Globals.difficult = 1
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
 	def _on_medium_pressed(self):
-		difficult = 1.5
+		#add Globals. forward
+		Globals.difficult = 1.5
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
 	def _on_hard_pressed(self):
-		difficult = 2
+		#add Globals. forward
+		Globals.difficult = 2
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
