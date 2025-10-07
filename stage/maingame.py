daf6c51ee8297fb17aa61d09502b3f59c878088b
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-08 05:24:46 +0700

    Update maingame.py
    Add monster encounter

--- stage/maingame.py
+++ stage/maingame.py
@@ -7,19 +7,23 @@ from .random_events import random_event_picker
 @gdclass
 class maingame(Control):
 	def _ready(self):
+		'''
+		Game init
+		'''
+		self.get_node("Back_Button").disabled = False
+		self.get_node("Next_Button").disabled = False
+		self.get_node("Flee_Button").disabled = False
+		self.get_node("Battle_Button").visible = False
+		self.get_node("Flee_Button").visible = True
+		self.get_node("Next_Button").visible = True
+		self.get_node("Monster_control").visible = False
 		self.get_node("dead_panel").visible = False
 		Globals.init_character()
 		print(f"Level: {Globals.level}")
 		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
 		print(f"Total EXP: {Globals.exp_total}")
 
-		### Residue code block (Awaiting delete) ###
-	def _on_skip_pressed(self):
-		print("skip")
-		#check difficult
-		print(Globals.difficult)
-		### Residue code block (Awaiting delete) ###
-
+	# Possibly the heart of the game sit here in this function
 	def _on_next_pressed(self):
 		print("next")
 		events = {
@@ -32,11 +36,53 @@ class maingame(Control):
 		}
 		selected_event = random_event_picker.pick_random_event(events)
 		print(selected_event)		# Debug
+		match selected_event:
+			case 'monster_encounter':
+				# Still need monster randomization
+				print('monster')
+				self.get_node("Flee_Button").visible = False
+				self.get_node("Next_Button").visible = False
+				self.get_node("Battle_Button").visible = True
+				self.get_node("Monster_control").visible = True
+			case 'treasure_chest':
+				print('treasure')
+			case 'rest_stop':
+				print('rest_stop')
 		
+		### --- Debug ---
 		Globals.gain_exp(50)
 		print(f"Level: {Globals.level}")
 		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
 		print(f"Total EXP: {Globals.exp_total}")
+		### --- Debug ---
+	
+	# Passing enemy HP logic to someone else in the team
+	def _on_battle_button_pressed(self):
+		# For now it will be one-hit kill for all enemies
+		print("attack")
+		self.get_node("Battle_Button").visible = False
+		self.get_node("Flee_Button").visible = True
+		self.get_node("Next_Button").visible = True
+		self.get_node("Monster_control").visible = False
+		
+		
+	def _on_flee_button_pressed(self):
+		print("skip")
+		remain_hp = Globals.skip_penalty()
+		print(remain_hp)
+		if remain_hp < 0:
+			print("died")
+			self.get_node("Back_Button").disabled = True
+			self.get_node("Next_Button").disabled = True
+			self.get_node("Flee_Button").disabled = True
+			self.get_node("dead_panel").visible = True
+
+		### Residue code block (Awaiting delete) ###
+	def _on_skip_pressed(self):
+		print("skip")
+		#check difficult
+		print(Globals.difficult)
+		### Residue code block (Awaiting delete) ###
 
 	def _on_back_button_pressed(self):
 		print("Back")
@@ -44,12 +90,4 @@ class maingame(Control):
 
 	def _on_died_back_to_menu_pressed(self):
 		self.get_tree().change_scene_to_file("res://main_menu.tscn")
-
-
-	def _on_flee_button_pressed(self):
-		print("skip")
-		remain_hp = Globals.skip_penalty()
-		print(remain_hp)
-		if remain_hp < 0:
-			print("died")
-			self.get_node("dead_panel").visible = True
+		
