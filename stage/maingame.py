7facdfda523f671d0717b49580f14d359ae33162
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-07 15:39:49 +0700

    Update maingame.py
    Add randomly selecting event based on probability

--- stage/maingame.py
+++ stage/maingame.py
@@ -1,6 +1,8 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
 from .charactor import Globals
+from .random_events import random_event_picker
+
 
 @gdclass
 class maingame(Control):
@@ -11,13 +13,26 @@ class maingame(Control):
 		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
 		print(f"Total EXP: {Globals.exp_total}")
 
+		### Residue code block (Awaiting delete) ###
 	def _on_skip_pressed(self):
 		print("skip")
 		#check difficult
 		print(Globals.difficult)
+		### Residue code block (Awaiting delete) ###
 
 	def _on_next_pressed(self):
 		print("next")
+		events = {
+			# Event structure is:
+			# 'Event name' : probability(int, float)
+			"monster_encounter": 60,
+			"treasure_chest": 30,
+			"rest_stop": 10
+			# Add more events here
+		}
+		selected_event = random_event_picker.pick_random_event(events)
+		print(selected_event)		# Debug
+		
 		Globals.gain_exp(50)
 		print(f"Level: {Globals.level}")
 		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
