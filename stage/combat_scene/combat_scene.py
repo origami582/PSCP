39d08b50156e857d34a90bd87041582b8bef28b6
Origami5822 <sodamm2kp@gmail.com>
2025-10-08 20:49:06 +0700

    fix print combat

--- stage/combat_scene/combat_scene.py
+++ stage/combat_scene/combat_scene.py
@@ -1,7 +1,7 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
 
-
 @gdclass
-class Attack(Control):
-	
+class combat_scene(Control):
+	def _on_attack_pressed(self):
+		print("test")
