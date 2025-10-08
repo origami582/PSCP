8f57bd28401213d1c00bffe2fa3963127c9d131f
Origami5822 <sodamm2kp@gmail.com>
2025-10-08 20:01:53 +0700

    Level

---
+++ stage/Level.py
@@ -0,0 +1,17 @@
+from py4godot.classes import gdclass
+#lebel for text box output
+from py4godot.classes.Label import Label
+from .charactor import Globals
+
+@gdclass
+class Level(Label):
+	def _ready(self):
+		self.update_level()
+
+	def _process(self, delta):
+		#loop print update_level
+		self.update_level()
+
+	def update_level(self):
+		#print update_level
+		self.text = f"Level: {Globals.level}"
