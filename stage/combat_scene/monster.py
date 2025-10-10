d8b2f2c50d4fa297f8a215f2f5e2c823712d8fd0
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-11 02:44:11 +0700

    Update monster.py
    Add meaningful debug text

--- stage/combat_scene/monster.py
+++ stage/combat_scene/monster.py
@@ -4,4 +4,4 @@ from .monster_stat import Monster
 @gdclass
 class Monster: #use base_stat from monster_status and scale here
 	slime_hp = Monster.base_hp * Monster.slime
-	print(slime_hp)
+	print(f"Slime HP: {slime_hp}")
