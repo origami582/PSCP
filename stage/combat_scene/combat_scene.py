f63ea1004da549551773be50ea26b6f560d53bfd
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-14 14:18:41 +0700

    Reformatting indentation

--- stage/combat_scene/combat_scene.py
+++ stage/combat_scene/combat_scene.py
@@ -4,43 +4,54 @@ from .monster import Monster
 from ..charactor import Globals
 from py4godot.classes.Label import Label
 from ..maingame import maingame
+from ..random_events import random_event_picker
+
 
 @gdclass
 class combat_scene(Control):
-    def _ready(self):
-        self.get_node("Attack_Button").disabled = False
-        self.get_node("Flee_Button").disabled = False
-        self.get_node("dead_screen").visible = False
+	def _ready(self):
+		self.get_node("Attack_Button").disabled = False
+		self.get_node("Flee_Button").disabled = False
+		self.get_node("dead_screen").visible = False
+		# Randomly pick monster from avaliable pool and change here
+		self.pick_monster()
 
-    def player_died(self):
-        '''call this function when player is dead'''
-        self.get_node("Attack_Button").disabled = True
-        self.get_node("Flee_Button").disabled = True
-        self.get_node("dead_screen").visible = True
 
-    Cuurent_HP = Monster.slime_hp
-    def _on_attack_button_pressed(self):        #button pressed to attack monster
-        if Monster.slime_hp <= 0:       #check monster hp for change scence
-            print("Monster Defeated")
-            print(f"You gained 50 EXP!!!")
-            Globals.room += 1
-            Globals.gain_exp(50)        #Gained EXP after defeated monster
-            # Status report will always trigger after returning to maingame (stage1.tscn) :)
-            self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
-        Monster.slime_hp -= Globals.strength        #damage
-        combat_scene.Cuurent_HP = Monster.slime_hp
-        print(f"HP remainig: {combat_scene.Cuurent_HP}")
+	def player_died(self):
+		'''call this function when player is dead'''
+		self.get_node("Attack_Button").disabled = True
+		self.get_node("Flee_Button").disabled = True
+		self.get_node("dead_screen").visible = True
 
-    def _on_flee_button_pressed(self):
-        remain_hp = Globals.flee_penalty()
-        print(f"Debug player hp: {remain_hp}")
-        if remain_hp <= 0:
-            print("died")
-            self.player_died()      # call function above when player is dead
-            ### Maybe call result scence here?
-        else:
-            # Player survived, return to main stage
-            self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+	def pick_monster(self):
+		'''picking random monster and display on screen'''
+		selected_encounter = random_event_picker.pick_random_encounter()
+		print(selected_encounter)	# Debug
 
-    def _on_died_back_to_menu_pressed(self):
-        self.get_tree().change_scene_to_file("res://stage/main_menu.tscn")
+
+	Cuurent_HP = Monster.slime_hp
+	def _on_attack_button_pressed(self):        #button pressed to attack monster
+		if Monster.slime_hp <= 0:       #check monster hp for change scence
+			print("Monster Defeated")
+			print(f"You gained 50 EXP!!!")
+			Globals.room += 1
+			Globals.gain_exp(50)        #Gained EXP after defeated monster
+			# Status report will always trigger after returning to maingame (stage1.tscn) :)
+			self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+		Monster.slime_hp -= Globals.strength        #damage
+		combat_scene.Cuurent_HP = Monster.slime_hp
+		print(f"HP remainig: {combat_scene.Cuurent_HP}")
+
+	def _on_flee_button_pressed(self):
+		remain_hp = Globals.flee_penalty()
+		print(f"Debug player hp: {remain_hp}")
+		if remain_hp <= 0:
+			print("died")
+			self.player_died()      # call function above when player is dead
+			### Maybe call result scence here?
+		else:
+			# Player survived, return to main stage
+			self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+
+	def _on_died_back_to_menu_pressed(self):
+		self.get_tree().change_scene_to_file("res://stage/main_menu.tscn")
--- stage/maingame.py
+++ stage/maingame.py
@@ -6,57 +6,59 @@ from .random_events import random_event_picker
 
 @gdclass
 class maingame(Control):
-    def _ready(self):
-        '''
-        Game init
-        '''
-        self.get_node("Back_Button").disabled = False
-        self.get_node("Next_Button").disabled = False
-        self.get_node("Return_Button").disabled = False
-        # ONLY initialize if the character hasn't been set up yet (a simple check)
-        if Globals.max_hp is None:
-            Globals.init_character()
-        self.status_update()
+	def _ready(self):
+		'''
+		Game init
+		'''
+		self.get_node("Back_Button").disabled = False
+		self.get_node("Next_Button").disabled = False
+		self.get_node("Return_Button").disabled = False
+		# ONLY initialize if the character hasn't been set up yet (a simple check)
+		if Globals.max_hp is None:
+			Globals.init_character()
+		self.status_update()
 
-    def status_update(self):
-        '''for unify debuging purpose'''
-        print(f"Level: {Globals.level}")
-        print(f"HP: {Globals.actual_hp}/{Globals.max_hp}")
-        print(f"EXP: {Globals.exp}/{Globals.exp_req}")
-        print(f"Total EXP: {Globals.exp_total}")
+	def status_update(self):
+		'''for unify debuging purpose'''
+		print(f"Level: {Globals.level}")
+		print(f"HP: {Globals.actual_hp}/{Globals.max_hp}")
+		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
+		print(f"Total EXP: {Globals.exp_total}")
 
-    # Possibly the heart of the game sit here in this function
-    def _on_next_pressed(self):
-        print("next")
-        selected_event = random_event_picker.pick_random_event()
-        print(selected_event)       # Debug
-        match selected_event:
-            case 'monster_encounter':
-                # Still need monster randomization
-                print('monster')
-                self.get_tree().change_scene_to_file("res://stage/combat_scene/combat_scene.tscn")
-            case 'treasure_chest':
-                print('treasure')
-            case 'rest_stop':
-                print('rest_stop')
+	# Possibly the heart of the game sit here in this function
+	def _on_next_pressed(self):
+		print("next")
+		selected_event = random_event_picker.pick_random_event()
+		print(selected_event)       # Debug
+		match selected_event:
+			case 'monster_encounter':
+				# Still need monster randomization
+				print('monster')
+				self.get_tree().change_scene_to_file("res://stage/combat_scene/combat_scene.tscn")
+			case 'treasure_chest':
+				print('treasure')
+				# Change scene to tresure chest
+			case 'rest_stop':
+				print('rest_stop')
+				# Change scene to rest stop
 
-        # Only execute this block if the scene did NOT change
-        ### --- Debug ---
-        Globals.gain_exp(50) # Assuming you gain EXP from non-combat events
-        self.status_update() # Prints the HP *after* the non-combat event
-        ### --- Debug ---
+		# Only execute this block if the scene did NOT change
+		### --- Debug ---
+		Globals.gain_exp(50) # Assuming you gain EXP from non-combat events
+		self.status_update() # Prints the HP *after* the non-combat event
+		### --- Debug ---
 
-    # Idk reuturn to the begining or maybe rest stop and rest ig
-    def _on_return_button_pressed(self):
-        print("return")
+	# Idk reuturn to the begining or maybe rest stop and rest ig
+	def _on_return_button_pressed(self):
+		print("return")
 
-        ### Residue code block (Awaiting delete) ###
-    def _on_skip_pressed(self):
-        print("skip")
-        #check difficult
-        print(Globals.difficult)
-        ### Residue code block (Awaiting delete) ###
+		### Residue code block (Awaiting delete) ###
+	def _on_skip_pressed(self):
+		print("skip")
+		#check difficult
+		print(Globals.difficult)
+		### Residue code block (Awaiting delete) ###
 
-    def _on_back_button_pressed(self):
-        print("Back")
-        self.get_tree().change_scene_to_file("res://stage/main_menu.tscn")
+	def _on_back_button_pressed(self):
+		print("Back")
+		self.get_tree().change_scene_to_file("res://stage/main_menu.tscn")
