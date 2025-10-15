2064319a09cfbb03f836fd44fe24a28f8dce4a7a
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-16 05:49:25 +0700

    Update hp_monster.gd
    Update documentation

--- stage/combat_scene/hp_monster.gd
+++ stage/combat_scene/hp_monster.gd
@@ -1,5 +1,7 @@
 extends Label
 
+# It would be better if we work with label and display text in native GDscript
+
 # This function updates the HP label.
 # It takes the current HP and maximum HP as arguments.
 func update_hp(current_hp, max_hp):
