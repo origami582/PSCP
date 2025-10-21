a03e8aff0660bc7c2feb5da73cd2b58d1fd1ec0f
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-22 03:19:48 +0700

    Create popup.gd

---
+++ stage/UI/popup.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func ItemPopup(slot: Rect2i, item):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var correction
+
+	if mouse_pos.x <= get_viewport_rect().size.x/2:
+		correction = Vector2i(slot.size.x, 0)
+	else:
+		correction = -Vector2i(%ItemPopup.size.x, 0)
+
+	%ItemPopup.popup(Rect2i(slot.position + correction, %ItemPopup.size))
+
+func HideItemPopup():
+	%ItemPopup.hide()
