6a764eec4f8137630faad3a84ba96caff798794d
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-25 04:18:50 +0700

    Add loading from save functionality

--- main.py
+++ main.py
@@ -2,56 +2,67 @@ from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
 #Connect files to charactor
 from stage.charactor import Globals
+from stage.save_reload import load_game
+
 
 @gdclass
 class main(Control):
-	# popup close
+	def __init__(self):
+		# Don't forget to call the parent class's constructor!
+		super().__init__()
+		self.reload = load_game()
+
 	def _ready(self):
 		#ready game this pop up close
 		self.get_node("CanvasLayer").visible = False
-		self.get_node("Setting").visible = False 
-		#music 
+		self.get_node("Setting").visible = False
+		#music
 		self.music = self.get_node("AudioStreamPlayer")
+
 
 	def _on_newgame_pressed(self):
 		#open popup
 		self.get_node("Level_select").visible = True
-		Globals.hp = 100
-		Globals.strength = 10
-		
-	def _on_Continew_pressed(self):
+
+	def _on_Continue_pressed(self):
 		#pass
 		print("Continue")
+		self.reload.load()
+		if Globals.previous_scene_path != "" and Globals.player is not None:
+			self.get_tree().change_scene_to_file(Globals.previous_scene_path)
+		else:
+			print("No save presence.")
 
-	def _on_lederbord_pressed(self):
+	def _on_option_pressed(self):
 		#pass
 		self.get_node("Setting").visible = True
 
+	def _on_setting_back_pressed(self):
+		# Close setting popup
+		self.get_node("Setting").visible = False
+
 	def _on_closepop_pressed(self):
 		#close popup
-		self.get_node("Level_select").visible = False 
+		self.get_node("Level_select").visible = False
 
 	#exitgame
 	def _on_exitgame_pressed(self):
 		self.get_tree().quit()
 
-	#difficult select and var to scale with exp_gain and monster status 
+	#difficult select and var to scale with exp_gain and monster status
 	def _on_easy_pressed(self):
 		#add Globals. forward
-		Globals.difficult = 1
+		Globals.difficulty = 1
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
 	def _on_medium_pressed(self):
 		#add Globals. forward
-		Globals.difficult = 1.5
+		Globals.difficulty = 1.5
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
 	def _on_hard_pressed(self):
 		#add Globals. forward
-		Globals.difficult = 2
+		Globals.difficulty = 2
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
 
 	def _on_music_changed(self, value: float):
-		db_value = -80 + (value * 1.5) 
+		db_value = -80 + (value * 1.5)
 		self.music.volume_db = db_value
-
-	def _on_continew_pressed(self):
-		self.get_node("Setting").visible = False
