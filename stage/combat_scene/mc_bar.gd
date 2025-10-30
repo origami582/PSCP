1c386317e05f61451d92affe78bdc5d6e05d2403
Heartsteel-pong <kittapatpraisaleeforwork@gmail.com>
2025-10-31 04:21:00 +0700

    Mc_hp bar

---
+++ stage/combat_scene/mc_bar.gd
@@ -0,0 +1,5 @@
+extends ProgressBar
+
+func update_monster_bar(current_hp, max_hp):
+	max_value = max_hp
+	value = current_hp
---
+++ stage/combat_scene/mc_hp.gd
@@ -0,0 +1,5 @@
+extends Label
+
+func update_hp(current_hp, max_hp):
+	text = "HP: " + str(current_hp) + " / " + str(max_hp)
+	
