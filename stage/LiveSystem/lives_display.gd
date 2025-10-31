668826d0850506db4adfc8e60ec370b2f7b72cba
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-31 23:56:18 +0700

    massive update
    Add lives system, more monsters, and bosses

--- stage/LiveSystem/lives_display.gd
+++ stage/LiveSystem/lives_display.gd
@@ -1,23 +1,6 @@
 extends Node2D
 
-func _ready():
-	update_display(3)
-
-func update_display(count):
-	print("updated heart")
-	if Livecounter.live == 3 :
-		$Heart1.show()
-		$Heart2.show()
-		$Heart3.show()
-	elif Livecounter.live == 2:
-		$Heart1.show()
-		$Heart2.show()
-		$Heart3.hide()
-	elif Livecounter.live == 1:
-		$Heart1.show()
-		$Heart2.hide()
-		$Heart3.hide()
-	elif Livecounter.live == 0:
-		$Heart1.hide()
-		$Heart2.hide()
-		$Heart3.hide()
+func update_display(lives):
+	$Heart1.visible = (lives >= 1)
+	$Heart2.visible = (lives >= 2)
+	$Heart3.visible = (lives >= 3)
--- stage/combat_scene/combat_scene.py
+++ stage/combat_scene/combat_scene.py
@@ -1,11 +1,11 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
 from py4godot.classes.Label import Label
-from ..charactor import Character, Globals
+from py4godot.classes import Input
+from ..charactor import Globals
 from ..maingame import maingame
 from ..random_events import random_event_picker
 from .monster import Monster
-from py4godot.classes import Input
 
 @gdclass
 class combat_scene(Control):
@@ -14,7 +14,6 @@ class combat_scene(Control):
 		"""Initializes the combat scene by setting up UI and spawning a monster."""
 		self.get_node("Attack_Button").disabled = False # type: ignore
 		self.get_node("Flee_Button").disabled = False
-		self.get_node("dead_screen").visible = False
 		self.get_node("Textbox").visible = False
 
 
@@ -24,11 +23,17 @@ class combat_scene(Control):
 		self.player_hp_label = self.get_node("HP_player")
 		self.player_bar = self.get_node("Player_bar")
 		self.monster_node = self.get_node("Monster")
+		self.textbox_node = self.get_node("Textbox")
 
 		# Randomly pick monster from available pool and create an instance
 		selected_encounter = self.pick_monster()
-		self.monster = Monster().setup_monster(monster_type=selected_encounter)
-		self.monster_texture = self.monster.texture_path
+		if Globals.room == 9:
+			# Setup boss instead of normal monster
+			self.monster = Monster().setup_boss(boss_type=selected_encounter)
+			self.monster_texture = self.monster.texture_path
+		else:
+			self.monster = Monster().setup_monster(monster_type=selected_encounter)
+			self.monster_texture = self.monster.texture_path
 
 		# Change monster texture here
 		self.monster_node.call("update_texture", self.monster_texture)
@@ -43,20 +48,7 @@ class combat_scene(Control):
 
 	def player_died(self):
 		"""Disables combat buttons and shows the death screen."""
-		# self.get_node("Attack_Button").disabled = True
-		# self.get_node("Flee_Button").disabled = True
-		# self.get_node("dead_screen").visible = True
-		# Globals.player_lives -= 1 # minus player lives
-		# print(f"Player died! Lives remaining: {Globals.player_lives}") #debug
-		# if Globals.player_lives > 0:
-		# 	print("You Lose the fight but stand up again...")
-		# else :
-		# 	print("You are Wiped")
-
 		Globals.player.lifes -= 1
-		# call func4tion updateddisplay
-		#
-		#
 		Globals.player.actual_hp = Globals.player.max_hp
 		print("DEBUG: Player died - Live reducted by 1")
 		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
@@ -64,26 +56,28 @@ class combat_scene(Control):
 	def pick_monster(self):
 		"""Picks a random monster from the encounter pool."""
 		# Picking which monster encounter
-		selected_encounter = random_event_picker.pick_random_encounter()
+		if Globals.room == 9:
+			selected_encounter = random_event_picker.pick_random_boss()
+		else:
+			selected_encounter = random_event_picker.pick_random_encounter()
 		print(f"Encountering: {selected_encounter}")	# Debug
 		return selected_encounter
 
 	def _on_attack_button_pressed(self) -> None:
 		"""Handles the event when the player's attack button is pressed."""
 		# Get the textbox and its label, then update the text
-		textbox_node = self.get_node("Textbox")
-		textbox_node.visible = True
-		textbox_node.get_node("Text").call("clear_text")
+		self.textbox_node.visible = True
+		self.textbox_node.get_node("Text").call("clear_text")
 
 		# Player attacks monster
 		dice_face = random_event_picker.dice_roll()
-		textbox_node.get_node("Text").call("show_dice_roll", dice_face)
+		self.textbox_node.get_node("Text").call("show_dice_roll", dice_face)
 		player_damage = Globals.player.strength * dice_face
 		print(f"Dice roll: {dice_face}, Damage: {player_damage}")  # Debug
 		self.monster.take_damage(amount=player_damage)
 		self.get_node("Attack_Button").visible = False
 		self.get_node("Flee_Button").visible = False
-		textbox_node.get_node("Text").call("show_player_attack",
+		self.textbox_node.get_node("Text").call("show_player_attack",
 										   self.monster.monster_type,
 										   player_damage)
 		monster_node = self.get_node("Monster")
@@ -93,8 +87,21 @@ class combat_scene(Control):
 				monster_anim_player.play("hit_flash")
 
 		if self.monster.is_dead:
+			# Special check for Roland's second phase
+			if self.monster.monster_type == "Roland":
+				self.textbox_node.get_node("Text").call("show_roland_transform")
+
+				# Transform into The_Black_Silence
+				self.monster = Monster().setup_boss(boss_type="The_Black_Silence")
+				self.monster_node.call("update_texture", self.monster.texture_path)
+
+				# Update UI for the new phase and allow the player to act again
+				self.update_ui_elements()
+				# We don't set wait_for_next_scene, the textbox will clear on the next 'ui_accept'
+				return
+
 			# Monster is dead, show victory message and wait for player to continue.
-			textbox_node.get_node("Text").call("show_monster_dead",
+			self.textbox_node.get_node("Text").call("show_monster_dead",
 											   self.monster.monster_type,
 											   self.monster.exp_reward)
 			self.wait_for_next_scene = True
@@ -105,7 +112,7 @@ class combat_scene(Control):
 		# Monster attacks player, if it's still alive
 		monster_damage = self.monster.atk
 		Globals.player.actual_hp -= monster_damage
-		textbox_node.get_node("Text").call("show_monster_attack",
+		self.textbox_node.get_node("Text").call("show_monster_attack",
 										   self.monster.monster_type,
 										   monster_damage)
 		#animationformc
@@ -114,33 +121,27 @@ class combat_scene(Control):
 			self.player_died()
 			return # Stop further execution if player is defeated
 
-		# Check for game over
-		if Globals.player.lifes <= 0:
-			self.show_game_over_screen()
-
 		self.update_ui_elements()
 
 	def _on_flee_button_pressed(self):
 		"""Handles the event when the flee button is pressed."""
 		remain_hp = Globals.player.flee_penalty()
-		print(f"Debug player hp: {remain_hp}")
-		if remain_hp <= 0:
-			print("died")
-			self.player_died()      # call function above when player is dead
-			return
-
-		if Globals.player.lifes > 0:
-			# Player survived, return to stage1
-			self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
-		else:
-			self.show_game_over_screen()
-
-	def show_game_over_screen(self):
-		"""Displays the game over screen and hides combat UI."""
-		self.get_node("dead_screen").visible = True
-		self.get_node("Textbox").visible = False
 		self.get_node("Attack_Button").visible = False
 		self.get_node("Flee_Button").visible = False
+		self.wait_for_next_scene = True
+		self.textbox_node.visible = True
+		self.textbox_node.get_node("Text").call("clear_text")
+
+		print(f"Debug player hp: {remain_hp}")
+		if remain_hp <= 0:
+			self.textbox_node.get_node("Text").call("show_flee", True, self.monster.monster_type, 0)
+			print("died")
+			# We will wait for input before calling player_died()
+		else:
+			penalty = Globals.player.get_penalty()
+			# Player survived, return to stage1
+			self.textbox_node.get_node("Text").call("show_flee", False, self.monster.monster_type, penalty)
+			# We will wait for input before changing the scene
 
 	def update_ui_elements(self):
 		"""Updates all HP-related UI elements."""
@@ -149,26 +150,24 @@ class combat_scene(Control):
 		self.player_hp_label.call("update_hp", Globals.player.actual_hp, Globals.player.max_hp)
 		self.player_bar.call("update_player_bar", Globals.player.actual_hp, Globals.player.max_hp)
 
-	def _on_died_back_to_menu_pressed(self):
-		"""Handles returning to the main menu from the death screen."""
-		self.get_tree().change_scene_to_file("res://stage/main_menu.tscn")
-
 	def _input(self, event):
 		"""Handles player input for advancing dialogue in the textbox."""
 		if hasattr(self, "wait_for_next_scene") and self.wait_for_next_scene:
 			if event.is_action_pressed("ui_accept"):
 				self.wait_for_next_scene = False
-				print("Monster Defeated")
-				print(f"You gained {self.monster.exp_reward} EXP!!!")
-				Globals.room += 1
-				Globals.player.gain_exp(amount=self.monster.exp_reward)
-				self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+				if self.monster.is_dead:
+					print("Monster Defeated")
+					print(f"You gained {self.monster.exp_reward} EXP!!!")
+					Globals.room += 1
+					Globals.player.gain_exp(amount=self.monster.exp_reward)
+					self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+				elif Globals.player.actual_hp <= 0: # Died from fleeing
+					self.player_died()
+				else: # Fled successfully
+					self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+
 			return
 		if event.is_action_pressed("ui_accept"):
 			self.get_node("Textbox").visible = False
 			self.get_node("Attack_Button").visible = True
 			self.get_node("Flee_Button").visible = True
-
-	def _on_textbox_hidden(self):
-		"""Callback for when the textbox is hidden (currently unused)."""
-		pass
